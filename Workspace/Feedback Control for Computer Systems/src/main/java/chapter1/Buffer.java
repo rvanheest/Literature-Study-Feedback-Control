7a475d718b4fc15b45b3cb83eb3567d7f3e32a40
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2014-09-11 13:52:04 +0200

    NonRx implementation of chapter 1

---
+++ Workspace/Feedback Control for Computer Systems/src/main/java/chapter1/Buffer.java
@@ -0,0 +1,49 @@
+package chapter1;
+
+import java.util.Random;
+
+public class Buffer {
+
+	private final int maxWip;  // max work in progress (max #items in ready pool)
+	private final int maxFlow; // max flow from queue to downstream
+	
+	private int queued = 0;
+	private int wip = 0;
+
+	/**
+	 * Constructs a buffer with maximum size of the ready pool and a maximum flow out of the queue.
+	 * @param maxWip The maximum number of items in the ready pool
+	 * @param maxFlow The maximum number of items flowing out of the queue
+	 */
+	public Buffer(int maxWip, int maxFlow) {
+		this.maxWip = maxWip;
+		this.maxFlow = maxFlow;
+	}
+
+	/**
+	 * Performs <code>u</code> work upstream puts it in the ready pool. A random percentage is
+	 * taken out from the ready pool and put into the queue. Another random percentage is taken
+	 * from the queue and goes downstream.  
+	 * @param u the amount of work done upstream
+	 * @return the length of the queue after processing 1 timeunit.
+	 */
+	public int work(double u) {
+		Random random = new Random();
+		
+		// Add to ready pool
+		// 0 <= u <= maxWip
+		this.wip += Math.toIntExact(Math.min(Math.max(0, Math.round(u)), this.maxWip));
+		
+		// Transfer r items from ready pool to queue
+		int r = Math.toIntExact(Math.round(random.nextDouble() * this.wip));
+		this.wip -= r;
+		this.queued += r;
+		
+		// Release s items from queue to downstream process
+		int s = Math.toIntExact(Math.round(random.nextDouble() * this.maxFlow));
+		s = Math.min(s, this.queued); // s <= #items in queue
+		this.queued -= s;
+		
+		return this.queued;
+	}
+}
---
+++ Workspace/Feedback Control for Computer Systems/src/main/java/chapter1/Controller.java
@@ -0,0 +1,20 @@
+package chapter1;
+
+public class Controller {
+
+	private final double kp;
+	private final double ki;
+	
+	private int i = 0; // cumulative error
+
+	public Controller(double kp, double ki) {
+		this.kp = kp;
+		this.ki = ki;
+	}
+
+	public double work(int e) {
+		this.i += e;
+		
+		return this.kp * e + this.ki * this.i;
+	}
+}
