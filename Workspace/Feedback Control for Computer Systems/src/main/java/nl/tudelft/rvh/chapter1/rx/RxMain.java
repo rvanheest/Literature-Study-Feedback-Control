98c0ab270c4a6b8f923fccc2e7d6fb0d6a2af019
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2014-09-12 20:00:20 +0200

    Added and tested the open loop in Rx

--- Workspace/Feedback Control for Computer Systems/src/main/java/nl/tudelft/rvh/chapter1/rx/RxMain.java
+++ Workspace/Feedback Control for Computer Systems/src/main/java/nl/tudelft/rvh/chapter1/rx/RxMain.java
@@ -9,18 +9,19 @@ import rx.subjects.PublishSubject;
 public class RxMain {
 
 	public static void main(String[] args) {
-//		openLoop(new Buffer(2, 3), 5000).subscribe(System.out::println);
-		PublishSubject<Integer> closed = closedLoop(1.25, 0.01, new Buffer(10, 10), 5000);
-		closed.subscribe(System.out::println);
-		closed.onNext(0);
+		openLoop(new Buffer(10, 10), 5000).subscribe(System.out::println);
+		
+//		PublishSubject<Integer> closed = closedLoop(1.25, 0.01, new Buffer(10, 10), 5000);
+//		closed.subscribe(System.out::println);
+//		closed.onNext(0);
 	}
 
-//	public static Observable<Integer> openLoop(Buffer b, int tm) {
-//		Func1<Integer, Double> target = t -> 5.0;
-//
-//		return Observable.range(0, tm).map(target).map(b::work);
-//	}
-	
+	public static Observable<Integer> openLoop(Buffer b, int tm) {
+		Func1<Integer, Double> target = t -> 5.0;
+
+		return Observable.range(0, tm).map(target).map(b::work);
+	}
+
 	public static PublishSubject<Integer> closedLoop(double kp, double ki, Buffer b, int tm) {
 		Func1<Integer, Integer> target = t -> t < 100 ? 0 : t < 300 ? 50 : 10;
 		Func2<Integer, Integer, Double> control = (e, c) -> kp * e + ki * c;
