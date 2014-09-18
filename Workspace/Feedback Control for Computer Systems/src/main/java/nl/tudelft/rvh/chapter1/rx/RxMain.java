2ab686eabbb81b883b7aee8eeaf00b1deede229a
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2014-09-18 21:59:29 +0200

    Changes Rx implementation using Observable.create and ReplaySubject

--- Workspace/Feedback Control for Computer Systems/src/main/java/nl/tudelft/rvh/chapter1/rx/RxMain.java
+++ Workspace/Feedback Control for Computer Systems/src/main/java/nl/tudelft/rvh/chapter1/rx/RxMain.java
@@ -2,18 +2,16 @@ package nl.tudelft.rvh.chapter1.rx;
 
 import nl.tudelft.rvh.chapter1.Buffer;
 import rx.Observable;
+import rx.Subscriber;
 import rx.functions.Func1;
 import rx.functions.Func2;
-import rx.subjects.PublishSubject;
+import rx.subjects.ReplaySubject;
 
 public class RxMain {
 
 	public static void main(String[] args) {
-		openLoop(new Buffer(10, 10), 5000).subscribe(System.out::println);
-		
-//		PublishSubject<Integer> closed = closedLoop(1.25, 0.01, new Buffer(10, 10), 5000);
-//		closed.subscribe(System.out::println);
-//		closed.onNext(0);
+		// openLoop(new Buffer(10, 10), 5000).subscribe(System.out::println);
+		closedLoop(1.25, 0.01, new Buffer(10, 10), 5000).subscribe(System.out::println);
 	}
 
 	public static Observable<Integer> openLoop(Buffer b, int tm) {
@@ -22,18 +20,23 @@ public class RxMain {
 		return Observable.range(0, tm).map(target).map(b::work);
 	}
 
-	public static PublishSubject<Integer> closedLoop(double kp, double ki, Buffer b, int tm) {
-		Func1<Integer, Integer> target = t -> t < 100 ? 0 : t < 300 ? 50 : 10;
+	public static Observable<Integer> closedLoop(double kp, double ki, Buffer b, int tm) {
+		Func1<Integer, Integer> target = t -> t < 100 ? 0
+				: t < 300 ? 50
+						: 10;
 		Func2<Integer, Integer, Double> control = (e, c) -> kp * e + ki * c;
-		
-		PublishSubject<Integer> queueLength = PublishSubject.create();
-		
-		Observable<Integer> error = Observable.zip(Observable.range(0, tm).map(target),
-				queueLength, (tar, q) -> tar - q);
-		Observable.zip(error, error.scan((e, cum) -> e + cum), control)
-				.map(b::work)
-				.subscribe(queueLength::onNext);
-		
-		return queueLength;
+
+		return Observable.create((Subscriber<? super Integer> subscriber) -> {
+			ReplaySubject<Integer> queueLength = ReplaySubject.create();
+			queueLength.onNext(0);
+
+			Observable<Integer> error = Observable.zip(Observable.range(0, tm).map(target),
+					queueLength, (tar, q) -> tar - q);
+			Observable.zip(error, error.scan((e, cum) -> e + cum), control)
+					.map(b::work)
+					.subscribe(queueLength::onNext);
+
+			queueLength.skip(1).subscribe(subscriber);
+		});
 	}
 }
--- Workspace/Feedback Control for Computer Systems/src/main/java/nl/tudelft/rvh/chapter2/rx/RxMain.java
+++ Workspace/Feedback Control for Computer Systems/src/main/java/nl/tudelft/rvh/chapter2/rx/RxMain.java
@@ -1,31 +1,36 @@
 package nl.tudelft.rvh.chapter2.rx;
 
 import rx.Observable;
+import rx.Subscriber;
 import rx.functions.Func1;
-import rx.subjects.PublishSubject;
+import rx.subjects.ReplaySubject;
 
 public class RxMain {
 
 	public static void main(String[] args) {
-		PublishSubject<Double> exp = experiment(160);
-		exp.subscribe(System.out::println);
-		exp.onNext(0.0);
+		experiment(160).subscribe(System.out::println);
 	}
 
-	public static PublishSubject<Double> experiment(float k) {
+	public static Observable<Double> experiment(float k) {
 		Func1<Integer, Double> setPoint = time -> time < 50 ? 0.6
 				: time < 100 ? 0.8
-						: time < 150 ? 0.1 : 0.9;
-		Func1<Double, Double> cache = size -> size < 0 ? 0 : size > 100 ? 1 : size / 100;
+						: time < 150 ? 0.1
+								: 0.9;
+		Func1<Double, Double> cache = size -> size < 0 ? 0
+				: size > 100 ? 1
+						: size / 100;
 
-		PublishSubject<Double> y = PublishSubject.create();
+		return Observable.create((Subscriber<? super Double> subscriber) -> {
+			ReplaySubject<Double> hitrate = ReplaySubject.create();
+			hitrate.onNext(0.0);
 
-		Observable.zip(Observable.range(0, 200).map(setPoint), y, (a, b) -> a - b)
-				.scan((e, cum) -> e + cum)
-				.map(cum -> k * cum)
-				.map(cache)
-				.subscribe(y::onNext);
+			Observable.zip(Observable.range(0, 200).map(setPoint), hitrate, (a, b) -> a - b)
+					.scan((e, cum) -> e + cum)
+					.map(cum -> k * cum)
+					.map(cache)
+					.subscribe(hitrate::onNext);
 
-		return y;
+			hitrate.skip(1).subscribe(subscriber);
+		});
 	}
 }
