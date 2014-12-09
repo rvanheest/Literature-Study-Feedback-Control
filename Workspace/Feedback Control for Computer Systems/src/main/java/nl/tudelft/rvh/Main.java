7dfac96a4615df2df88158705dfe6940836ea24b
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2014-12-09 14:06:26 +0100

    Removed test tabs

--- Workspace/Feedback Control for Computer Systems/src/main/java/nl/tudelft/rvh/Main.java
+++ Workspace/Feedback Control for Computer Systems/src/main/java/nl/tudelft/rvh/Main.java
@@ -1,15 +1,14 @@
 package nl.tudelft.rvh;
 
-import nl.tudelft.rvh.chapter1.BufferClosed;
-import nl.tudelft.rvh.chapter1.BufferOpen;
-import nl.tudelft.rvh.chapter2.CacheCumulative;
-import nl.tudelft.rvh.chapter2.CacheNonCumulative;
-import nl.tudelft.rvh.scala.ScalaExampleTab;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.stage.Stage;
+import nl.tudelft.rvh.chapter1.BufferClosed;
+import nl.tudelft.rvh.chapter1.BufferOpen;
+import nl.tudelft.rvh.chapter2.CacheCumulative;
+import nl.tudelft.rvh.chapter2.CacheNonCumulative;
 
 public class Main extends Application {
 
@@ -17,10 +16,8 @@ public class Main extends Application {
 	public void start(Stage stage) {
 		TabPane tabPane = new TabPane();
 		tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
-		tabPane.getTabs().addAll(new ExampleTab(),
-				new BufferClosed(), new BufferOpen(),
-				new CacheCumulative(), new CacheNonCumulative(),
-				new ScalaExampleTab());
+		tabPane.getTabs().addAll(new CacheCumulative(), new CacheNonCumulative(),	// chapter 2
+				new BufferClosed(), new BufferOpen());	// chapter 1
 
 		stage.setScene(new Scene(tabPane, 800, 600));
 		stage.setTitle("Feedback Control Systems");
