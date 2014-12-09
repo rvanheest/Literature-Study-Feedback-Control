58de9bdcfc6751d4ce218a4592e9331b443d1b66
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2014-12-09 11:58:37 +0100

    Scala formatting

--- Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/scala/ScalaChartTab.scala
+++ Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/scala/ScalaChartTab.scala
@@ -5,13 +5,14 @@ import nl.tudelft.rvh.Tuple
 import rx.lang.scala.Observable
 import rx.lang.scala.JavaConversions
 
-abstract class ScalaChartTab(tabName: String, chartTitle: String, xName: String, yName: String) extends ChartTab(tabName, chartTitle, xName, yName) {
+abstract class ScalaChartTab(tabName: String, chartTitle: String, xName: String, yName: String)
+		extends ChartTab(tabName, chartTitle, xName, yName) {
 
-  def runSimulation(): rx.Observable[Tuple[Number, Number]] = {
-    val tuples = simulation().map(t => new Tuple(t._1, t._2))
-    JavaConversions.toJavaObservable(tuples)
-      .asInstanceOf[rx.Observable[Tuple[Number, Number]]]
-  }
+	def runSimulation(): rx.Observable[Tuple[Number, Number]] = {
+		val tuples = simulation().map(t => new Tuple(t._1, t._2))
+		JavaConversions.toJavaObservable(tuples)
+			.asInstanceOf[rx.Observable[Tuple[Number, Number]]]
+	}
 
-  def simulation(): Observable[(Number, Number)]
+	def simulation(): Observable[(Number, Number)]
 }
