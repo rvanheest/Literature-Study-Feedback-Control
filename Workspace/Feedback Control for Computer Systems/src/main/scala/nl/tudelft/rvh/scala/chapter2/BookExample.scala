c70987af645283ebb5f2b976516a69452b7bb429
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2014-12-30 15:25:12 +0100

    BookExample chap2 (in Scala - iterative FBS)

---
+++ Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/scala/chapter2/BookExample.scala
@@ -0,0 +1,23 @@
+package nl.tudelft.rvh.scala.chapter2
+
+object BookExample extends App {
+
+	def cache(size: Double): Double = math.max(0, math.min(1, size / 100))
+	
+	def setPoint(time: Long): Double = if (time < 30) 0.6 else if (time < 60) 0.8 else if (time < 90) 0.1 else 0.4
+	val gain = 160
+	
+	var hitrate = 0.0
+	var cumErr = 0.0
+	
+	println(0, hitrate)
+	
+	for (t <- 0 until 120) {
+		val trErr = setPoint(t) - hitrate
+		cumErr += trErr
+		val controlAction = gain * cumErr
+		hitrate = cache(controlAction)
+		
+		println(t + 1, hitrate)
+	}
+}
