a6d71af4474c26a78cd896b6eca7f0632665e2dc
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-01-09 15:18:15 +0100

    Formatting PI and PID controllers

--- Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/scala/chapter4/PID.scala
+++ Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/scala/chapter4/PID.scala
@@ -1,6 +1,11 @@
 package nl.tudelft.rvh.scala.chapter4
 
 class PID(val prop: Double = 0, val integral: Double = 0, val deriv: Double = 0, prevErr: Double = 0) {
-	def work(error: Double, DT: Double = 1): PID = new PID(error, integral + error, (error - prevErr) / DT, error)
-	def controlAction(kp: Double, ki: Double, kd: Double) = prop * kp + integral * ki + deriv * kd
+	def work(error: Double, DT: Double = 1): PID = {
+		new PID(error, integral + error, (error - prevErr) / DT, error)
+	}
+
+	def controlAction(kp: Double, ki: Double, kd: Double) = {
+		prop * kp + integral * ki + deriv * kd
+	}
 }
