0a3a4e934e90dfa3c57478e4f91576acd0311fed
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-01-19 14:31:46 +0100

    Added extra override to the Filter trait
    Just like in Controller and Plant

--- Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/scala/simulation/filters/Filter.scala
+++ Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/scala/simulation/filters/Filter.scala
@@ -2,4 +2,6 @@ package nl.tudelft.rvh.scala.simulation.filters
 
 import nl.tudelft.rvh.scala.simulation.Component
 
-trait Filter extends Component
+trait Filter extends Component {
+	override def update(u: Double): Filter
+}
