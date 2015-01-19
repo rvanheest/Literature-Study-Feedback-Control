eb97070325cd434aa76ba34191c3612fec9ec920
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-01-19 11:01:36 +0100

    Another controller

--- Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/scala/simulation/controller/DeadbandRelayContr.scala
+++ Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/scala/simulation/controller/DeadbandRelayContr.scala
@@ -1,14 +1,14 @@
 package nl.tudelft.rvh.scala.simulation.controller
 
-class DeadbandRelayContr(zone: Double, res: Double = 0) extends Controller {
+class DeadbandRelayController(zone: Double, res: Double = 0) extends Controller {
 
-	def update(error: Double): DeadbandRelayContr = {
+	def update(error: Double): DeadbandRelayController = {
 		if (error > zone)
-			new DeadbandRelayContr(zone, 1)
+			new DeadbandRelayController(zone, 1)
 		else if (error < -zone)
-			new DeadbandRelayContr(zone, -1)
+			new DeadbandRelayController(zone, -1)
 		else
-			new DeadbandRelayContr(zone, 0)
+			new DeadbandRelayController(zone, 0)
 	}
 
 	def action: Double = res
---
+++ Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/scala/simulation/controller/HysteresisRelayContr.scala
@@ -0,0 +1,14 @@
+package nl.tudelft.rvh.scala.simulation.controller
+
+class HysteresisRelayController(zone: Double, prev: Double = 0, res: Double = 0) extends Controller {
+
+	def update(error: Double): HysteresisRelayController = {
+		val u = if (error > prev)
+			if (error < zone) 0 else 1
+		else
+			if (error > -zone) 0 else -1
+		new HysteresisRelayController(zone, error, u)
+	}
+	
+	def action: Double = res
+}
