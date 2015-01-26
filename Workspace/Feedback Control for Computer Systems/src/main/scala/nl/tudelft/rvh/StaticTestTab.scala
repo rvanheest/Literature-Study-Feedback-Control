9799551a1bae711da93b72a301ebe075d6938928
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-01-26 17:21:55 +0100

    Added StaticTestTab and StepTestTab

---
+++ Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/StaticTestTab.scala
@@ -0,0 +1,110 @@
+package nl.tudelft.rvh
+
+import java.io.File
+
+import scala.collection.JavaConverters.asScalaBufferConverter
+
+import javafx.embed.swing.SwingFXUtils
+import javafx.event.ActionEvent
+import javafx.scene.SnapshotParameters
+import javafx.scene.chart.NumberAxis
+import javafx.scene.chart.ScatterChart
+import javafx.scene.chart.XYChart.Data
+import javafx.scene.chart.XYChart.Series
+import javafx.scene.control.Button
+import javafx.scene.control.Tab
+import javafx.scene.layout.HBox
+import javafx.scene.layout.VBox
+import javafx.stage.FileChooser
+import javafx.stage.FileChooser.ExtensionFilter
+import javax.imageio.ImageIO
+import nl.tudelft.rvh.rxjavafx.JavaFxScheduler
+import nl.tudelft.rvh.rxscalafx.Observables
+import rx.lang.scala.JavaConversions
+import rx.lang.scala.Observable
+
+abstract class StaticTestTab(tabName: String, chartTitle: String, xName: String, yName: String)(implicit DT: Double = 1.0) extends Tab(tabName) {
+
+	private val simulate = new Button("Start simulation")
+	private val print = new Button("Print data")
+	private val save = new Button("Save chart")
+	private val clear = new Button("Clear chart")
+
+	val chart = initChart(chartTitle, xName, yName)
+	chart setAnimated false
+
+	print setDisable true
+	save setDisable true
+
+	Observables.fromNodeEvents(simulate, ActionEvent.ACTION)
+		.doOnNext(event => {
+			simulate setDisable true
+			clear setDisable true
+			print setDisable true
+			save setDisable true
+		})
+		.map(_ => new Series[Number, Number])
+		.doOnNext(_.setName(this.seriesName))
+		.doOnNext(chart.getData add _)
+		.flatMap(series => this.runSimulation
+			.map(tuple => new Data(tuple._1, tuple._2))
+			.observeOn(JavaConversions.javaSchedulerToScalaScheduler(JavaFxScheduler.getInstance))
+			.doOnNext(series.getData add _)
+			.doOnCompleted({
+				simulate setDisable false
+				clear setDisable false
+				print setDisable false
+				save setDisable false
+			}))
+		.doOnError(_ printStackTrace)
+		.subscribe
+
+	Observables.fromNodeEvents(clear, ActionEvent.ACTION)
+		.subscribe(_ => chart.getData.clear)
+
+	Observables.fromNodeEvents(print, ActionEvent.ACTION)
+		.flatMap(_ => Observable.from(chart.getData asScala))
+		.map(series => series.getData.asScala.map(data => data.getXValue + ", " + data.getYValue)
+			.foldLeft(series.getName + ":")((sum, current) => sum + "\n" + current))
+		.subscribe(println(_))
+
+	Observables.fromNodeEvents(save, ActionEvent.ACTION)
+		.map(_ => chart.snapshot(new SnapshotParameters, null))
+		.map(SwingFXUtils.fromFXImage(_, null))
+		.flatMap(img => getFile.map(f => ImageIO.write(img, "png", f)))
+		.subscribe
+
+	this setContent new VBox(chart, bottomBox)
+
+	def initChart(title: String, xName: String, yName: String) = {
+		val xAxis = new NumberAxis
+		val yAxis = new NumberAxis
+		val chart = new ScatterChart(xAxis, yAxis)
+
+		xAxis setLabel xName
+		yAxis setLabel yName
+		chart setTitle title
+
+		chart
+	}
+
+	def getFile: Observable[File] = {
+		Observable(subscriber => {
+			val fileChooser = new FileChooser
+			fileChooser setTitle "Save image"
+			fileChooser.getExtensionFilters add new ExtensionFilter("PNG files (*.png)", "*.png")
+
+			Option(fileChooser showSaveDialog null)
+				.map(f => if (f.getPath endsWith ".png") f else new File(f.getPath + ".png"))
+				.foreach(subscriber onNext _)
+		})
+	}
+
+	def bottomBox = new HBox(simulate, clear, print, save)
+
+	def seriesName: String
+
+	def runSimulation: Observable[(Number, Number)] = simulation.onBackpressureBuffer.asInstanceOf[Observable[(Number, Number)]]
+
+	def simulation: Observable[(Double, Double)]
+}
