b691075b6259a0e3cee276fb966a703e971a7c0e
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-03-04 15:03:39 +0100

    Fixed bug in ServerPool

--- Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/simulation/Plant.scala
+++ Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/simulation/Plant.scala
@@ -85,7 +85,7 @@ class ServerPool(n: Int, queue: Double = 0, server: () => Double, load: () => Do
 		}
 		else {
 			val nNew = math.max(0, u)
-			val completed = math.min(l, (0 until nNew).map { _ => server() }.scan(0.0){ (_ + _) }.takeWhile { _ < l }.last)
+			val completed = math.min((0 until nNew).map { _ => server() }.sum, l)
 			new ServerPool(nNew, l - completed, server, load, completed / l)
 		}
 	}
