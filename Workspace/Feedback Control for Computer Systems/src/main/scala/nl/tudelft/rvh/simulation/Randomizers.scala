d87adb8fb9f92e863a492f45ba56f399db9ed592
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-03-04 12:57:55 +0100

    Reimplemented the gammavariate and made it tailrecursive

--- Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/simulation/Randomizers.scala
+++ Workspace/Feedback Control for Computer Systems/src/main/scala/nl/tudelft/rvh/simulation/Randomizers.scala
@@ -1,57 +1,63 @@
 package nl.tudelft.rvh.simulation
 
 import scala.util.Random
+import scala.annotation.tailrec
 
 object Randomizers {
 
 	def gaussian(mean: Double, stdDev: Double) = new Random().nextGaussian() * stdDev + mean
 
 	def gammavariate(alpha: Double, beta: Double): Double = {
+		@tailrec def whileLoop1(ainv: Double, bbb: Double, ccc: Double): Double = {
+			val u1 = math.random
+			if (1e-7 < u1 && u1 < .9999999) {
+				val u2 = 1.0 - math.random
+				val v = math.log(u1 / (1.0 - u1)) / ainv
+				val x = alpha * math.exp(v)
+				val z = u1 * u1 * u2
+				val r = bbb + ccc * v - x
+				if (r + 1.0 + math.log(4.5) - 4.5 * z >= 0 || r >= math.log(z)) {
+					return x * beta
+				}
+			}
+			
+			whileLoop1(ainv, bbb, ccc)
+		}
+		
+		@tailrec def getRandomValueLargerThan(minimum: Double): Double = {
+			val u = math.random
+			if (u > minimum) u
+			else getRandomValueLargerThan(minimum)
+		}
+		
+		@tailrec def whileLoop2: Double = {
+			val u = math.random
+			val b = (math.E + alpha) / math.E
+			val p = b * u
+			val x = if (p <= 1.0) math.pow(p, (1.0 / alpha)) else -math.log((b - p) / alpha)
+			val u1 = math.random
+			if (p > 1.0 && u1 <= math.pow(x, alpha - 1.0) || u1 <= math.exp(-x)) {
+				return x * beta
+			}
+			return whileLoop2
+		}
+		
 		if (alpha <= 0.0 || beta <= 0.0) {
 			return -1
 		}
 
-		val random = Random
 		if (alpha > 1.0) {
 			val ainv = math.sqrt(2.0 * alpha - 1.0)
 			val bbb = alpha - math.log(4)
 			val ccc = alpha + ainv
 
-			while (true) {
-				val u1 = random.nextGaussian() // ?? TODO
-				if (1e-7 < u1 && u1 < .9999999) {
-					val u2 = 1.0 - random.nextGaussian() // ?? TODO
-					val v = math.log(u1 / (1.0 - u1)) / ainv
-					val x = alpha * math.exp(v)
-					val z = u1 * u1 * u2
-					val r = bbb + ccc * v - x
-					if (r + math.log(4.5) - 3.5 * z >= 0 || r >= math.log(z)) {
-						return x * beta
-					}
-				}
-			}
-			return 1.0
+			whileLoop1(ainv, bbb, ccc)
 		}
 		else if (alpha == 1.0) {
-			var u = random.nextGaussian()
-			while (u <= 1e-7) {
-				u = random.nextGaussian()
-			}
+			val u = getRandomValueLargerThan(1e-7)
 			return -math.log(u) * beta
 		}
-		else {
-			while (true) {
-				val u = random.nextGaussian()
-				val b = (math.E + alpha) / math.E
-				val p = b * u
-				val x = if (p <= 10) math.pow(p, (1.0 / alpha)) else -math.log((b - p) / alpha)
-				val u1 = random.nextGaussian()
-				if (p > 1.0 && u1 <= math.pow(x, alpha - 1.0) || u1 <= math.exp(-x)) {
-					return x * beta
-				}
-			}
-			return 1.0
-		}
+		else whileLoop2
 	}
 
 	def betavariate(alpha: Double, beta: Double) = {
